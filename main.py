17d76d065d5e1ad26fdf4320b8f197f4361af792
Abdurahmon <abdurahmonhoshimov2004@gmail.com>
2024-03-05 23:30:21 +0500

    main fayli commit qilindi

--- main.py
+++ main.py
@@ -3,3 +3,13 @@ from bot import start_bot_polling
 
 if __name__ == "__main__":
     start_bot_polling()
+
+byt = bytearray()
+while True:
+    name = input("ism kiriting: ")
+    if name == "stop":
+        break
+    lastname = input("familiya kiriting: ")
+    yoshi = input("yosh: ")
+    byt.extend(bytes(f'ismi: {name} familiyasi: {lastname} yoshi: {yoshi}'.encode('utf-8')))
+    print(byt)
